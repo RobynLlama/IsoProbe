b3ab3a5e8b576976ad716f6d0b55d134d649e3b0
Robyn <rnixon98@gmail.com>
2025-07-23 12:40:39 -0500

    feat: peek does hexdump style output by default
    can still use --raw to override this to old behavior

--- src/PSXtractor/Commands/Interactive/CommandPeekFile.cs
+++ src/PSXtractor/Commands/Interactive/CommandPeekFile.cs
@@ -9,14 +9,33 @@ public class CommandPeekFile : ICommandRunner
 {
   public string CommandName => "peek";
 
-  public string CommandUsage => "peeks at 128 bytes of data from the start of the given file\n  Usage: peek <fileName>";
+  public string CommandUsage => "peeks at 128 bytes of data from the start of the given file\n  Usage: peek <fileName> OR peek --raw <filename>";
 
   public bool Execute(string[] args)
   {
+    bool rawMode = false;
+    string fileName = string.Empty;
+
     if (args.Length == 0)
     {
       Console.WriteLine(CommandUsage);
       return true;
+    }
+    else if (args.Length == 1)
+    {
+      fileName = args[0];
+    }
+    else if (args.Length == 2)
+    {
+      if (args[0].Equals("--raw", StringComparison.InvariantCultureIgnoreCase))
+        rawMode = true;
+      else
+      {
+        Console.WriteLine($"error: unknown flag {args[0]}");
+        return false;
+      }
+
+      fileName = args[1];
     }
 
     if (InteractiveDispatcher.Environment.LoadedMedia is null)
@@ -25,7 +44,7 @@ public class CommandPeekFile : ICommandRunner
       return false;
     }
 
-    var fullIdentifier = InteractiveDispatcher.Environment.ResolveUserPath(args[0]);
+    var fullIdentifier = InteractiveDispatcher.Environment.ResolveUserPath(fileName);
     var item = InteractiveDispatcher.Environment.LoadedMedia.GetRecordFromPath(fullIdentifier);
 
     if (item is null || item.FlagIsDirectory)
@@ -42,8 +61,43 @@ public class CommandPeekFile : ICommandRunner
     : (int)contents.Length;
 
     byte[] data = reader.ReadBytes(dataToRead);
+
     Console.WriteLine("----------");
-    Console.WriteLine(Encoding.ASCII.GetString(data));
+
+    if (!rawMode)
+    {
+      var bytesWritten = 0;
+      var bytesLine = 16;
+      Console.Write($"0x0000: ");
+
+      foreach (var thing in data)
+      {
+        Console.Write($"0x{thing:X2} ");
+        bytesWritten++;
+
+        if (bytesWritten % bytesLine == 0)
+        {
+          Console.Write(" | ");
+          for (int i = bytesWritten - bytesLine; i < bytesWritten; i++)
+          {
+            byte current = data[i];
+            if (current >= 32 && current <= 126)
+              Console.Write((char)current);
+            else
+              Console.Write('.');
+
+          }
+
+          Console.WriteLine();
+
+          if (bytesWritten != data.Length)
+            Console.Write($"0x{bytesWritten:X4}: ");
+        }
+      }
+    }
+    else
+      Console.WriteLine(Encoding.ASCII.GetString(data));
+
     Console.WriteLine("----------");
 
     return true;
